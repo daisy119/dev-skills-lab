437490a8d741d438e704c77e0296083a46f84c1d
Jia Wang <daisy-a.119@hotmail.com>
2024-01-11 00:36:22 -0500

    update the data directory

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const devSkills = [
+  {text: 'Javascript', done: false, _id: 125223},
+  {text: 'React', done: false, _id: 127904},
+  {text: 'Python', done: false, _id: 139608},
+]
+
+export { 
+	todos
+}
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import logger from 'morgan'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as usersRouter } from './routes/skills.js'
 
 // create the express app
 const app = express()
