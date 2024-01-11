a5a59d720fbb349358b21bffd5b2e070584a3d2f
Jia Wang <daisy-a.119@hotmail.com>
2024-01-11 00:51:01 -0500

    update skillsRouter

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,9 +1,9 @@
-const devSkills = [
+const skills = [
   {text: 'Javascript', done: false, _id: 125223},
   {text: 'React', done: false, _id: 127904},
   {text: 'Python', done: false, _id: 139608},
 ]
 
 export { 
-	todos
+	skills
 }
