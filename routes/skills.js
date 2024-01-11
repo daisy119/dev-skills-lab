ee938ffbedc57724f5b3d769f62cd0f5df1eded6
Jia Wang <daisy-a.119@hotmail.com>
2024-01-11 01:34:13 -0500

    set up index controller

--- routes/skills.js
+++ routes/skills.js
@@ -1,13 +1,11 @@
 import { Router } from 'express'
-import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controller/skills.js'
 
 const router = Router()
 
 // GET localhost:3000/skills
-router.get('/', function(req, res) {
-  res.render('skills/index',{
-    skills: skills
-  })
-})
+//skillsCtrl is an imported object
+router.get('/', skillsCtrl.index)
+
 
 export { router }
