5ffb67dea88118f6bb69fa233ea923f76f0df4f0
Jia Wang <daisy-a.119@hotmail.com>
2024-01-13 18:26:14 -0500

    add Skill Model

---
+++ models/todo.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+	
+const skillSchema = new Schema({
+  text: String,
+  done: Boolean,
+})
+
+// Compile the schema into a model and export it. Model is capitalized
+const Skill = mongoose.model('Skill', todoSchema)
+
+export {
+  Todo
+}
